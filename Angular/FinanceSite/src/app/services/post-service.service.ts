3ca5979aa08afdb0932dda73e401b540b7ed9e49
Ben <benjamin.johnston1998@gmail.com>
2020-12-23 18:47:14 -0500

    Removed Mailgun API from project

--- Angular/FinanceSite/src/app/services/post-service.service.ts
+++ Angular/FinanceSite/src/app/services/post-service.service.ts
@@ -12,19 +12,10 @@ import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 export class PostServiceService {
 
-  private mailgunURL: string = 'https://api.mailgun.net/v3/mg.benjohnstongames.com';
-  private apiKey:string ='8bdc17d6cf9c7aba8698cbed0773bbcc-e5da0167-dd8d04f0';
+  
 
   constructor(private http: HttpClient,private uServ:UserService) { }
   u:User = new User(0,"username","","","","");
-  sendPassword(username:string)
-  {
-    this.uServ.getUserByUsername(new User(0,username,"","","","")).subscribe(
-      data => {this.u = data},
-      );
-      console.log(this.u);
- 
-
-  }
+  
     
 }
